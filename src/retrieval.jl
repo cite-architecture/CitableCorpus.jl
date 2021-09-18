1123b7b25a14c94e86218160443117299ebf52d3
Neel Smith <dnsmith.neel@gmail.com>
2021-09-18 05:34:52 -0400

    Implement and test retrieval functions

--- src/retrieval.jl
+++ src/retrieval.jl
@@ -20,24 +20,66 @@ end
 """Retrieve citable passages identified by `u` from a DataFrame of 
 citable passages.
 Returns a (possibly empty) Vector of `CitablePassage`s.
+
 $(SIGNATURES)
 """
 function retrieve(u::CtsUrn, psgdf::DataFrame)
-    @warn("Not yet implemetned")
-    nothing
+    isrange(u) ? retrieve_range(u, psgdf) : retrieve_simple(u, psgdf)
 end
 
+"""Find citable passages in `psgdf` contained by `u`.
+
+$(SIGNATURES)
+"""
 function retrieve_simple(u::CtsUrn, psgdf::DataFrame)
-    @warn("Not yet implemetned")
-    nothing
+    psgs = []
+    for r in eachrow(psgdf)
+        if urncontains(u, r.urn)
+            push!(psgs, CitablePassage(r.urn, r.text))
+        end
+    end
+    psgs
 end
 
+
+"""Find citable passages in `psgdf` contained by `u`.
+
+$(SIGNATURES)
+"""
 function retrieve_range(u::CtsUrn, psgdf::DataFrame)
-    @warn("Not yet implemetned")
-    nothing
+    urn1 = addpassage(u, range_begin(u))
+    indices1 = row_indices(urn1, psgdf)
+
+
+
+    urn2 = addpassage(u, range_end(u))
+    indices2 = row_indices(urn2, psgdf)
+    
+    if isempty(indices1) || isempty(indices2)
+        []
+    else
+        psgs = []
+        startidx = indices1[1]
+        endidx = indices2[end]
+        range = psgdf[startidx:endidx, :]
+        for r in eachrow(range)
+            push!(psgs, CitablePassage(r.urn, r.text))
+        end
+        psgs
+    end
 end
 
+
+"""Find row indices in `psgdf` for passages contained by `u`.
+
+$(SIGNATURES)
+"""
 function row_indices(u::CtsUrn, psgdf::DataFrame)
-    @warn("Not yet implemetned")
-    nothing
+    indices = []
+    for r in eachrow(psgdf)
+        if urncontains(u, r.urn)
+            push!(indices, rownumber(r))
+        end
+    end
+    indices
 end
